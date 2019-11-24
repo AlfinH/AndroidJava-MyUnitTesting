6a36202a25bd7d52be42bcb21f68ab087d3207f5
AlfinH <47842410+AlfinH@users.noreply.github.com>
2019-11-24 17:19:03 +0700

    Contoh Pengujian 3
    pengujian dengan menggunakan mock

--- app/src/test/java/com/alfin/myunittesting/MainViewModelTest.java
+++ app/src/test/java/com/alfin/myunittesting/MainViewModelTest.java
@@ -5,12 +5,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class MainViewModelTest {
 
     private MainViewModel mainViewModel;
     private CuboidModel cuboidModel;
-
     private final double dummyLength = 12.0;
     private final double dummyWidth = 7.0;
     private final double dummyHeight = 6.0;
@@ -41,12 +42,35 @@ public class MainViewModelTest {
         double volume = mainViewModel.getCircumference();
         assertEquals(dummyCircumference, volume, 0.0001);
     }
+
     @Test
     public void tesSurfaceArea() {
         cuboidModel = new CuboidModel();
         mainViewModel = new MainViewModel(cuboidModel);
         mainViewModel.save(dummyWidth, dummyLength, dummyHeight);
         double volume = mainViewModel.getSurfaceArea();
+        assertEquals(dummySurfaceArea, volume, 0.0001);
+    }
+
+    @Test
+    public void testMockVolume() {
+        when(mainViewModel.getVolume()).thenReturn(dummyVolume);
+        double volume = mainViewModel.getVolume();
+        verify(cuboidModel).getVolume();
+        assertEquals(dummyVolume, volume, 0.0001);
+    }
+    @Test
+    public void testMockCircumference() {
+        when(mainViewModel.getCircumference()).thenReturn(dummyCircumference);
+        double volume = mainViewModel.getCircumference();
+        verify(cuboidModel).getCircumference();
+        assertEquals(dummyCircumference, volume, 0.0001);
+    }
+    @Test
+    public void testMockSurfaceArea() {
+        when(mainViewModel.getSurfaceArea()).thenReturn(dummySurfaceArea);
+        double volume = mainViewModel.getSurfaceArea();
+        verify(cuboidModel).getSurfaceArea();
         assertEquals(dummySurfaceArea, volume, 0.0001);
     }
 }
