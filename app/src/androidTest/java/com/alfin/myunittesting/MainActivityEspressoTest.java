343e306b4bca67a95011f6e7d0a6fd59cbfd7b99
AlfinH <47842410+AlfinH@users.noreply.github.com>
2019-12-30 10:01:51 +0700

    Done!
    Tests Passed: 4

--- app/src/androidTest/java/com/alfin/myunittesting/MainActivityEspressoTest.java
+++ app/src/androidTest/java/com/alfin/myunittesting/MainActivityEspressoTest.java
@@ -12,6 +12,7 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
@@ -42,5 +43,50 @@ public class MainActivityEspressoTest {
         onView(withId(R.id.btn_calculate_circumference)).perform(click());
         onView(withId(R.id.tv_result)).check(matches(isDisplayed()));
         onView(withId(R.id.tv_result)).check(matches(withText(dummyCircumference)));
+    }
+
+    @Test
+    public void getSurfaceArea() {
+        onView(withId(R.id.edt_length)).perform(typeText(dummyLength), closeSoftKeyboard());
+        onView(withId(R.id.edt_width)).perform(typeText(dummyWidth), closeSoftKeyboard());
+        onView(withId(R.id.edt_height)).perform(typeText(dummyHeight), closeSoftKeyboard());
+        onView(withId(R.id.btn_save)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_save)).perform(click());
+        onView(withId(R.id.btn_calculate_surface_area)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_calculate_surface_area)).perform(click());
+        onView(withId(R.id.tv_result)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_result)).check(matches(withText(dummySurfaceArea)));
+    }
+    @Test
+    public void getVolume() {
+        onView(withId(R.id.edt_length)).perform(typeText(dummyLength), closeSoftKeyboard());
+        onView(withId(R.id.edt_width)).perform(typeText(dummyWidth), closeSoftKeyboard());
+        onView(withId(R.id.edt_height)).perform(typeText(dummyHeight), closeSoftKeyboard());
+        onView(withId(R.id.btn_save)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_save)).perform(click());
+        onView(withId(R.id.btn_calculate_volume)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_calculate_volume)).perform(click());
+        onView(withId(R.id.tv_result)).check(matches(isDisplayed()));
+        onView(withId(R.id.tv_result)).check(matches(withText(dummyVolume)));
+    }
+    @Test
+    public void emptyInput() {
+        onView(withId(R.id.edt_length)).perform(typeText(emptyInput), closeSoftKeyboard());
+        onView(withId(R.id.btn_save)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_save)).perform(click());
+        onView(withId(R.id.edt_length)).check(matches(hasErrorText(fieldEmpty)));
+        onView(withId(R.id.edt_length)).perform(typeText(dummyLength), closeSoftKeyboard());
+        onView(withId(R.id.edt_width)).perform(typeText(emptyInput), closeSoftKeyboard());
+        onView(withId(R.id.btn_save)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_save)).perform(click());
+        onView(withId(R.id.edt_width)).check(matches(hasErrorText(fieldEmpty)));
+        onView(withId(R.id.edt_width)).perform(typeText(dummyWidth), closeSoftKeyboard());
+        onView(withId(R.id.edt_height)).perform(typeText(emptyInput), closeSoftKeyboard());
+        onView(withId(R.id.btn_save)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_save)).perform(click());
+        onView(withId(R.id.edt_height)).check(matches(hasErrorText(fieldEmpty)));
+        onView(withId(R.id.edt_height)).perform(typeText(dummyHeight), closeSoftKeyboard());
+        onView(withId(R.id.btn_save)).check(matches(isDisplayed()));
+        onView(withId(R.id.btn_save)).perform(click());
     }
 }
